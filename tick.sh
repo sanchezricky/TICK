0f09e378231d9faae7a92b5a4433314039296a08
Ricky Sanchez <rickysanchez4130@gmail.com>
2018-12-25 21:41:46 -0800

    Initial tick script

---
+++ tick.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euox pipefail
+
+# Adding TICK stack repo
+curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+source /etc/lsb-release
+echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+
