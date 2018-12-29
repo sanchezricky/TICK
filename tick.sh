b66c6cc292de8320eb09db16218d5759f940fad1
Ricky Sanchez <rickysanchez4130@gmail.com>
2018-12-28 18:14:45 -0800

    Update apt

--- tick.sh
+++ tick.sh
@@ -7,3 +7,5 @@ curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
 source /etc/lsb-release
 echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
 
+# Update APT
+sudo apt-get update
