a60c5ea7a68418dd55022b2b5e268b6626c2d895
Ricky Sanchez <rickysanchez4130@gmail.com>
2018-12-30 22:14:49 -0800

    Added spaces between adding the tick stack repo

--- tick.sh
+++ tick.sh
@@ -4,7 +4,9 @@ set -euox pipefail
 
 # Adding TICK stack repo
 curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+
 source /etc/lsb-release
+
 echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
 
 # Update APT
@@ -12,3 +14,6 @@ sudo apt-get update
 
 # Influxdb install
 sudo apt-get install influxdb
+
+# Starting influxdb
+sudo systemctl start influxdb
